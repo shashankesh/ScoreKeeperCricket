aec9fcb3596d63bee4fc89d89a86012db564a86d
shashankesh <shashankesh@gmail.com>
2018-03-07 21:45:55 +0530

    Initial commit

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.scorekeeper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -25,17 +26,50 @@ public class MainActivity extends AppCompatActivity {
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
 
         {
-            public void onCheckedChanged (CompoundButton buttonView,boolean isChecked){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // The toggle is enabled
                     try {
+                        Button btn = (Button) findViewById(R.id.run10);
+                        btn.setText(String.valueOf(String.format("+1 extra")));
+                        Button btn1 = (Button) findViewById(R.id.run12);
+                        btn1.setText(String.valueOf(String.format("no run")));
 
-                            setA = true;
-                            setB = false;
-                            for (int i = 0; i < 6; i++) {
-                                v[i].setEnabled(true);
-                                v[i].setClickable(true);
-                            }
+                        Button btn2 = (Button) findViewById(R.id.run11);
+                        btn2.setText(String.valueOf(String.format("wicket")));
+
+
+                        Button btn3 = (Button) findViewById(R.id.run18);
+                        btn3.setText(String.valueOf(String.format("four")));
+
+                        Button btn4 = (Button) findViewById(R.id.run19);
+                        btn4.setText(String.valueOf(String.format("six")));
+                        Button btn5 = (Button) findViewById(R.id.run17);
+                        btn5.setText(String.valueOf(String.format("run")));
+
+                        Button btn6 = (Button) findViewById(R.id.run4);
+                        btn6.setText(String.valueOf(String.format("+1 extra")));
+
+                        Button btn7 = (Button) findViewById(R.id.run16);
+                        btn7.setText(String.valueOf(String.format("no run")));
+
+                        Button btn8 = (Button) findViewById(R.id.run15);
+                        btn8.setText(String.valueOf(String.format("wicket")));
+
+                        Button btn9 = (Button) findViewById(R.id.run2);
+                        btn9.setText(String.valueOf(String.format("four")));
+
+                        Button btn0 = (Button) findViewById(R.id.run3);
+                        btn0.setText(String.valueOf(String.format("six")));
+
+                        Button btn11 = (Button) findViewById(R.id.run1);
+                        btn11.setText(String.valueOf(String.format("run")));
+                        setA = true;
+                        setB = false;
+                        for (int i = 0; i < 6; i++) {
+                            v[i].setEnabled(true);
+                            v[i].setClickable(true);
+                        }
 
                     } catch (Exception ex) {
                         display_score(run, wk, ov, bl, "");
@@ -44,12 +78,46 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     // The toggle is disabled
                     try {
-                            setA = false;
-                            setB = true;
-                            for (int i = 6; i < 12; i++) {
-                                v[i].setEnabled(true);
-                                v[i].setClickable(true);
-                            }
+                        Button btn = (Button) findViewById(R.id.run10);
+                        btn.setText(String.valueOf(String.format("+1 extra")));
+                        Button btn1 = (Button) findViewById(R.id.run12);
+                        btn1.setText(String.valueOf(String.format("no run")));
+
+                        Button btn2 = (Button) findViewById(R.id.run11);
+                        btn2.setText(String.valueOf(String.format("wicket")));
+
+
+                        Button btn3 = (Button) findViewById(R.id.run18);
+                        btn3.setText(String.valueOf(String.format("four")));
+
+                        Button btn4 = (Button) findViewById(R.id.run19);
+                        btn4.setText(String.valueOf(String.format("six")));
+                        Button btn5 = (Button) findViewById(R.id.run17);
+                        btn5.setText(String.valueOf(String.format("run")));
+
+                        Button btn6 = (Button) findViewById(R.id.run4);
+                        btn6.setText(String.valueOf(String.format("+1 extra")));
+
+                        Button btn7 = (Button) findViewById(R.id.run16);
+                        btn7.setText(String.valueOf(String.format("no run")));
+
+                        Button btn8 = (Button) findViewById(R.id.run15);
+                        btn8.setText(String.valueOf(String.format("wicket")));
+
+                        Button btn9 = (Button) findViewById(R.id.run2);
+                        btn9.setText(String.valueOf(String.format("four")));
+
+                        Button btn0 = (Button) findViewById(R.id.run3);
+                        btn0.setText(String.valueOf(String.format("six")));
+
+                        Button btn11 = (Button) findViewById(R.id.run1);
+                        btn11.setText(String.valueOf(String.format("run")));
+                        setA = false;
+                        setB = true;
+                        for (int i = 6; i < 12; i++) {
+                            v[i].setEnabled(true);
+                            v[i].setClickable(true);
+                        }
 
 
                     } catch (Exception ex) {
@@ -64,11 +132,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void add1(View view) {
         if (wk > 9 || (ov > ov_limit) || setA == false) {
+            Button btn = (Button) findViewById(R.id.run1);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setClickable(false);
             view.setEnabled(false);
             v[0] = view;
         } else {
-
+            Button btn = (Button) findViewById(R.id.run1);
+            btn.setText(String.valueOf(String.format("run")));
             run++;
             bl++;
             if (bl == 6) {
@@ -82,11 +153,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void add6(View view) {
         if (wk > 9 || (ov > ov_limit) || setA == false) {
+            Button btn = (Button) findViewById(R.id.run3);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setClickable(false);
             view.setEnabled(false);
             v[1] = view;
 
         } else {
+            Button btn = (Button) findViewById(R.id.run3);
+            btn.setText(String.valueOf(String.format("six")));
 
             run = run + 6;
             bl++;
@@ -102,11 +177,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void add4(View view) {
         if (wk > 9 || (ov > ov_limit) || setA == false) {
+            Button btn = (Button) findViewById(R.id.run2);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setClickable(false);
             view.setEnabled(false);
             v[2] = view;
         } else {
-
+            Button btn = (Button) findViewById(R.id.run2);
+            btn.setText(String.valueOf(String.format("four")));
             run = run + 4;
             bl++;
             if (bl == 6) {
@@ -120,10 +198,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void wck(View view) {
         if (wk > 9 || (ov > ov_limit) || setA == false) {
+            Button btn = (Button) findViewById(R.id.run15);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setClickable(false);
             view.setEnabled(false);
             v[3] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run15);
+            btn.setText(String.valueOf(String.format("wicket")));
 
             wk++;
             bl++;
@@ -137,10 +219,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void noRun(View view) {
         if (wk > 9 || (ov > ov_limit) || setA == false) {
+            Button btn = (Button) findViewById(R.id.run16);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setClickable(false);
             view.setEnabled(false);
             v[4] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run16);
+            btn.setText(String.valueOf(String.format("no run")));
 
             bl++;
             if (bl == 6) {
@@ -153,11 +239,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void extra(View view) {
         if (wk > 9 || (ov > ov_limit) || setA == false) {
+            Button btn = (Button) findViewById(R.id.run4);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setClickable(false);
             view.setEnabled(false);
             v[5] = view;
         } else {
-
+            Button btn = (Button) findViewById(R.id.run4);
+            btn.setText(String.valueOf(String.format("+1 extra")));
             run++;
             if (bl == 6) {
                 bl = 0;
@@ -181,10 +270,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void add1B(View view) {
         if (wkB > 9 || (ovB > ov_limit) || setB == false) {
+            Button btn = (Button) findViewById(R.id.run17);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setEnabled(false);
             view.setClickable(false);
             v[6] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run17);
+            btn.setText(String.valueOf(String.format("run")));
             runB++;
             blB++;
             if (blB == 6) {
@@ -198,10 +291,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void add6B(View view) {
         if (wkB > 9 || (ovB > ov_limit) || setB == false) {
+            Button btn = (Button) findViewById(R.id.run19);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setEnabled(false);
             view.setClickable(false);
             v[7] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run19);
+            btn.setText(String.valueOf(String.format("six")));
             runB = runB + 6;
             blB++;
             if (blB == 6) {
@@ -215,10 +312,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void add4B(View view) {
         if (wkB > 9 || (ovB > ov_limit) || setB == false) {
+            Button btn = (Button) findViewById(R.id.run18);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setEnabled(false);
             view.setClickable(false);
             v[8] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run18);
+            btn.setText(String.valueOf(String.format("four")));
             runB = runB + 4;
             blB++;
             if (blB == 6) {
@@ -232,10 +333,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void wckB(View view) {
         if (wkB > 9 || (ovB > ov_limit) || setB == false) {
+            Button btn = (Button) findViewById(R.id.run11);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setEnabled(false);
             view.setClickable(false);
             v[9] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run11);
+            btn.setText(String.valueOf(String.format("wicket")));
             wkB++;
             blB++;
             if (blB == 6) {
@@ -248,10 +353,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void noRunB(View view) {
         if (wkB > 9 || (ovB > ov_limit) || setB == false) {
+            Button btn = (Button) findViewById(R.id.run12);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setEnabled(false);
             view.setClickable(false);
             v[10] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run12);
+            btn.setText(String.valueOf(String.format("no run")));
             blB++;
             if (blB == 6) {
                 blB = 0;
@@ -263,10 +372,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void extraB(View view) {
         if (wkB > 9 || (ovB > ov_limit) || setB == false) {
+            Button btn = (Button) findViewById(R.id.run10);
+            btn.setText(String.valueOf(String.format("Over")));
             view.setEnabled(false);
             view.setClickable(false);
             v[11] = view;
         } else {
+            Button btn = (Button) findViewById(R.id.run10);
+            btn.setText(String.valueOf(String.format("+1 extra")));
             runB++;
             if (blB == 6) {
                 blB = 0;
@@ -308,6 +421,43 @@ public class MainActivity extends AppCompatActivity {
                 display_score(run, wk, ov, bl, "loser");
                 display_scoreB(runB, wkB, ovB, blB, "winner");
             }
+
+            Button btn = (Button) findViewById(R.id.run10);
+            btn.setText(String.valueOf(String.format("+1 extra")));
+            Button btn1 = (Button) findViewById(R.id.run12);
+            btn1.setText(String.valueOf(String.format("no run")));
+
+            Button btn2 = (Button) findViewById(R.id.run11);
+            btn2.setText(String.valueOf(String.format("wicket")));
+
+
+            Button btn3 = (Button) findViewById(R.id.run18);
+            btn3.setText(String.valueOf(String.format("four")));
+
+            Button btn4 = (Button) findViewById(R.id.run19);
+            btn4.setText(String.valueOf(String.format("six")));
+            Button btn5 = (Button) findViewById(R.id.run17);
+            btn5.setText(String.valueOf(String.format("run")));
+
+            Button btn6 = (Button) findViewById(R.id.run4);
+            btn6.setText(String.valueOf(String.format("+1 extra")));
+
+            Button btn7 = (Button) findViewById(R.id.run16);
+            btn7.setText(String.valueOf(String.format("no run")));
+
+            Button btn8 = (Button) findViewById(R.id.run15);
+            btn8.setText(String.valueOf(String.format("wicket")));
+
+            Button btn9 = (Button) findViewById(R.id.run2);
+            btn9.setText(String.valueOf(String.format("four")));
+
+            Button btn0 = (Button) findViewById(R.id.run3);
+            btn0.setText(String.valueOf(String.format("six")));
+
+            Button btn11 = (Button) findViewById(R.id.run1);
+            btn11.setText(String.valueOf(String.format("run")));
+
+
             run = 0;
             wk = 0;
             ov = 0;
